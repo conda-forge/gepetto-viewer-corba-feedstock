8cbd1f85f5da2a7afe9e3cf55d6c673ee5fedaa5
Yann de Mont-Marin <yann.montmarin@gmail.com>
2026-03-17 17:16:04 +0100

    Remove omniidl export from build script

--- recipe/build.sh
+++ recipe/build.sh
@@ -1,7 +1,4 @@
 #!/bin/sh
-# Tell omniidl to use python back-end
-export PYTHONPATH=$BUILD_PREFIX/lib/python$PY_VER/site-packages/:$BUILD_PREFIX/lib/python$PY_VER/site-packages/omniidl_be/
-
 mkdir build
 cd build
 cmake ${CMAKE_ARGS} .. \
@@ -10,7 +7,6 @@ cmake ${CMAKE_ARGS} .. \
       -DCMAKE_INSTALL_PREFIX=$PREFIX \
       -DPYTHON_SITELIB=$PREFIX/lib/python$PY_VER/site-packages \
       -DCMAKE_INSTALL_LIBDIR=lib \
-      -DBUILD_UNIT_TESTS=OFF \
-      -DOMNIIDL=$BUILD_PREFIX/bin/omniidl
+      -DBUILD_UNIT_TESTS=OFF
 make -j${CPU_COUNT}
 make install
