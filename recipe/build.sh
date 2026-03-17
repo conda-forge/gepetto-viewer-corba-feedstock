6eeea5ec62e2de368f09e91c7ba0833596aa24a0
Yann de Mont-Marin <yann.montmarin@gmail.com>
2026-03-17 16:43:15 +0100

    Add OMNIIDL option to build script

--- recipe/build.sh
+++ recipe/build.sh
@@ -10,6 +10,7 @@ cmake ${CMAKE_ARGS} .. \
       -DCMAKE_INSTALL_PREFIX=$PREFIX \
       -DPYTHON_SITELIB=$PREFIX/lib/python$PY_VER/site-packages \
       -DCMAKE_INSTALL_LIBDIR=lib \
-      -DBUILD_UNIT_TESTS=OFF
+      -DBUILD_UNIT_TESTS=OFF \
+      -DOMNIIDL=$BUILD_PREFIX/bin/omniidl
 make -j${CPU_COUNT}
 make install
